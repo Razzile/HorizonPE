53576f1a0c63ccee1ad4161f75da96676157f3e5
Razzile <comfycallum@gmail.com>
2014-07-21 21:42:43 +0100

    uhmm yea
    added my beloved settings framework and a few new mods. updated said
    mods to use the framework

--- Hooks.cpp
+++ Hooks.cpp
@@ -1,43 +1,68 @@
 #include <substrate.h>
+#include "Settings.h"
 
 #define Hook(x,y,z) MSHookFunction(MSFindSymbol(NULL, x), (void*)y, (void**)&z)
 #define Sym(x) MSFindSymbol(NULL, x)
 __attribute__((constructor)) void InitHooks();
 
+/* definition of player abilities */
 struct Abilities {
-	bool test1;
-	bool test2;
-	bool test3;
-	bool test4;
+	bool invulnerable;
+	bool flying;
+	bool mayfly;
+	bool instabuild;
 };
 
-float (*GetWalkingSpeedModifier)(void *self);
-void (*Hurt)(void *self);
+/* definition of original functions */
+namespace Originals 
+{
+	float (*Player_GetWalkingSpeedModifier)(void *self);
+	void (*Player_Hurt)(void *self, void *entity, int damage);
+	void (*Player_normalTick)(void *self);
 
-void (*Mob_JumpFromGround)(void *self, float height);
+	void (*Mob_JumpFromGround)(void *self, float height);
 
-float (*HatchetItem_GetDestroySpeed)(void *ItemInstance, void *tile);
-float (*PickaxeItem_GetDestroySpeed)(void *ItemInstance, void *tile);
-float (*Item_GetDestroySpeed)(void *ItemInstance, void *tile);
-float (*DiggerItem_GetDestroySpeed)(void *ItemInstance, void *tile);
-float (*ShearsItem_GetDestroySpeed)(void *ItemInstance, void *tile);
-float (*WeaponItem_GetDestroySpeed)(void *ItemInstance, void *tile);
+	bool (*Entity_IsInWater)(void *self);
+	bool (*Entity_IsInWaterOrRain)(void *self);
 
-void (*SurvivalMode_InitAbilities)(Abilities&);
+	float (*HatchetItem_GetDestroySpeed)(void *ItemInstance, void *tile);
+	float (*PickaxeItem_GetDestroySpeed)(void *ItemInstance, void *tile);
+	float (*Item_GetDestroySpeed)(void *ItemInstance, void *tile);
+	float (*DiggerItem_GetDestroySpeed)(void *ItemInstance, void *tile);
+	float (*ShearsItem_GetDestroySpeed)(void *ItemInstance, void *tile);
+	float (*WeaponItem_GetDestroySpeed)(void *ItemInstance, void *tile);
+}
 
-
+/* our mod implementations */
 namespace Hooks 
 {
 	namespace Player 
 	{
 		float GetWalkingSpeedModifier(void *self)
 		{
-			return 2.0f;
+			return horizonSettings["kSpeed"];
 		}
 
-		void Hurt(void *self)
+		void Hurt(void *self, void *entity, int damage)
 		{
-			// empty for no implementation
+			bool invulnerable = horizonSettings["kInvulnerable"];
+			if(invulnerable)
+			{
+				return;
+			}
+			return Originals::Player_Hurt(self, entity, damage);
+		}
+
+		void normalTick(void *self)
+		{
+			bool fly = horizonSettings["kFly"];
+			if(fly)
+			{
+				Abilities *dataPtr = (Abilities*)self;
+				Abilities *abilities = &dataPtr[0xC6C/sizeof(Abilities)];
+				abilities->mayfly = true;
+			}
+			return Originals::Player_normalTick(self);
 		}
 	}
 	namespace Mob
@@ -45,42 +70,48 @@ namespace Hooks
 		void JumpFromGround(void *self)
 		{
 			float *dataPtr = (float*)self;
-			dataPtr[0x4C/sizeof(float)] = 2.0f;
+			dataPtr[0x4C/sizeof(float)] = horizonSettings["kJump"];
+		}
+	}
+	namespace Entity 
+	{
+		bool IsInWater(void *self)
+		{
+			bool enabled = horizonSettings["kWater"];
+			return (enabled == true) ? true : Originals::Entity_IsInWater(self);
+		}
+
+		bool IsInWaterOrRain(void *self)
+		{
+			bool enabled = horizonSettings["kWater"];
+			return (enabled == true) ? true : Originals::Entity_IsInWaterOrRain(self);
 		}
 	}
 	namespace Items
 	{
 		float HatchetItem_GetDestroySpeed(void *ItemInstance, void *tile)
 		{
-			return 100.0f;
+			return horizonSettings["kDestroy"];
 		}
 		float PickaxeItem_GetDestroySpeed(void *ItemInstance, void *tile)
 		{
-			return 100.0f;
+			return horizonSettings["kDestroy"];
 		}
 		float Item_GetDestroySpeed(void *ItemInstance, void *tile)
 		{
-			return 100.0f;
+			return horizonSettings["kDestroy"];
 		}
 		float DiggerItem_GetDestroySpeed(void *ItemInstance, void *tile)
 		{
-			return 100.0f;
+			return horizonSettings["kDestroy"];
 		}
 		float ShearsItem_GetDestroySpeed(void *ItemInstance, void *tile)
 		{
-			return 100.0f;
+			return horizonSettings["kDestroy"];
 		}
 		float WeaponItem_GetDestroySpeed(void *ItemInstance, void *tile)
 		{
-			return 100.0f;
-		}
-	}
-	namespace SurvivalMode
-	{
-		void InitAbilities(void *self, Abilities *abilities)
-		{
-			int *test = (int*)abilities;
-			test[0] = 12345678;
+			return horizonSettings["kDestroy"];
 		}
 	}
 }
@@ -88,11 +119,16 @@ namespace Hooks
 void InitHooks()
 {
 	using namespace Hooks;
+	using namespace Originals;
 
-	Hook("__ZN6Player23getWalkingSpeedModifierEv", Player::GetWalkingSpeedModifier, GetWalkingSpeedModifier);
-	Hook("__ZN6Player4hurtEP6Entityi", Player::Hurt, Hurt);
+	Hook("__ZN6Player23getWalkingSpeedModifierEv", Player::GetWalkingSpeedModifier, Player_GetWalkingSpeedModifier);
+	Hook("__ZN6Player4hurtEP6Entityi", Player::Hurt, Player_Hurt);
+	Hook("__ZN11LocalPlayer10normalTickEv", Player::normalTick, Player_normalTick);
 
 	Hook("__ZN3Mob14jumpFromGroundEv", Mob::JumpFromGround, Mob_JumpFromGround);
+
+	Hook("__ZNK6Entity9isInWaterEv", Entity::IsInWater, Entity_IsInWater);
+	Hook("__ZNK6Entity9isInWaterEv", Entity::IsInWaterOrRain, Entity_IsInWaterOrRain);
 
 	Hook("__ZN11HatchetItem15getDestroySpeedEP12ItemInstanceP4Tile", Items::HatchetItem_GetDestroySpeed, HatchetItem_GetDestroySpeed);
 	Hook("__ZN11PickaxeItem15getDestroySpeedEP12ItemInstanceP4Tile", Items::HatchetItem_GetDestroySpeed, HatchetItem_GetDestroySpeed);
@@ -101,6 +137,6 @@ void InitHooks()
 	Hook("__ZN10ShearsItem15getDestroySpeedEP12ItemInstanceP4Tile", Items::ShearsItem_GetDestroySpeed, ShearsItem_GetDestroySpeed);
 	Hook("__ZN10WeaponItem15getDestroySpeedEP12ItemInstanceP4Tile", Items::WeaponItem_GetDestroySpeed, WeaponItem_GetDestroySpeed);
 
-	Hook("__ZN12SurvivalMode13initAbilitiesER9Abilities", SurvivalMode::InitAbilities, SurvivalMode_InitAbilities);
+	// Hook("__ZN12SurvivalMode13initAbilitiesER9Abilities", SurvivalMode::InitAbilities, SurvivalMode_InitAbilities);
 
 }
